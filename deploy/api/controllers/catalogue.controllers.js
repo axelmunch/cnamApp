03cfc81b2da680a1c2f1c2f137fc316e4a7510f4
Axel Munch <92171197+axelmunch@users.noreply.github.com>
2023-12-10 16:51:51 +0100

    TP06

--- deploy/api/controllers/catalogue.controllers.js
+++ deploy/api/controllers/catalogue.controllers.js
@@ -1,13 +1,43 @@
+const catalogue = [
+  { ref: "A", name: "Smartphone", price: 899.99 },
+  { ref: "B", name: "Laptop", price: 619.99 },
+  { ref: "C", name: "Headphones", price: 299.99 },
+  { ref: "D", name: "Smartwatch", price: 199.99 },
+  { ref: "E", name: "Robot vacuum cleaner", price: 335.88 },
+  { ref: "F", name: "VR headset", price: 349.99 },
+  { ref: "G", name: "Bicycle", price: 600 },
+  { ref: "H", name: "Printer", price: 120 },
+  { ref: "I", name: "Drone", price: 199.99 },
+  { ref: "J", name: "Shower gel", price: 12.5 },
+];
 
 exports.get = (req, res) => {
-        const catalogue = [
-		{ref:"X001", titre : "Linux", prix : 10},
-		{ref:"X002", titre : "Angular", prix : 20}
-		];
-		
-	
-	res.setHeader('Content-Type', 'application/json');
-      
-    res.send(catalogue);
-   };    
+  res.setHeader("Content-Type", "application/json");
 
+  // Articles recommandés
+  res.send(catalogue);
+};
+
+exports.search = (req, res) => {
+  let searchText = req.query.q ?? "";
+  searchText = searchText.toLowerCase();
+  let priceInf = req.query.priceInf ?? 0;
+
+  if (searchText == "" && priceInf == 0) {
+    return this.get(req, res);
+  }
+
+  res.setHeader("Content-Type", "application/json");
+
+  // Filtrer
+  let search = catalogue.filter(
+    (item) =>
+      item.name.toLowerCase().includes(searchText) ||
+      item.price.toString().includes(searchText)
+  );
+  if (priceInf != 0) {
+    search = search.filter((item) => item.price <= priceInf);
+  }
+
+  res.send(search);
+};
--- deploy/api/controllers/utilisateur.controllers.js
+++ deploy/api/controllers/utilisateur.controllers.js
@@ -3,6 +3,16 @@ const { ACCESS_TOKEN_SECRET }  = require ("../config.js");
 
 const jwt = require('jsonwebtoken');
 
+let utilisateurs = [
+    {
+        nom: "Maurice",
+        prenom: "Emmanuel",
+        login: "emma",
+        email : "emmanuel.maurice@gmail.com",
+        password : "toto"
+    }
+]
+
 function generateAccessToken(user) {
     return jwt.sign(user, ACCESS_TOKEN_SECRET, { expiresIn: '365d' });
   }
@@ -14,19 +24,28 @@ exports.login = (req, res) => {
     password: req.body.password
   };
 
+  console.log(req.body);
+
   // Test
   let pattern = /^[A-Za-z0-9]{1,20}$/;
   if (pattern.test(utilisateur.login) && pattern.test(utilisateur.password)) {
 
         const uuid = uuidv4 ();
-        const utilisateur = {
-          nom: "martin",
-          prenom: "jean",
-          login: "marsstin",
-          email : "martin.jean@gmail.com",
-          password : "toto",
-          id : uuid
-        };
+        let utilisateur;
+
+        for (let i=0; i<utilisateurs.length; i++) {
+            if (utilisateurs[i].login == req.body.login && utilisateurs[i].password == req.body.password) {
+                utilisateur = utilisateurs[i];
+                break;
+            }
+        }
+        if(!utilisateur) {
+            res.status(401).send({
+                message: "Erreur d'authentification"
+            });
+            return;
+        }
+        utilisateur.id = uuid;
 
         const user = {
           id: utilisateur.id,
@@ -45,4 +64,36 @@ exports.login = (req, res) => {
     };    
 };
 
+exports.create = (req, res) => {
+    const input = {
+        nom: req.body.nom,
+        prenom: req.body.prenom,
+        email: req.body.email,
+        login: req.body.login,
+        password: req.body.password
+    };
+  
+    console.log(req.body);
+  
+    // Test
+    let pattern = /^[A-Za-z0-9]{1,20}$/;
+    let valid = true;
+    for(let i = 0; i < utilisateurs.length; i++) {
+        if (utilisateurs[i].login == input.login) {
+            valid = false;
+            break;
+        }
+    }
 
+    if(!valid)
+    {
+        res.status(401).send({
+            message: "Erreur de création"
+        });
+        return;
+    }
+
+    utilisateurs.push(input);
+
+    res.send(input);
+  };
