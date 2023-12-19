027afbb14aaa95c1764520029485686f92c311e5
Axel Munch <92171197+axelmunch@users.noreply.github.com>
2023-12-19 20:36:50 +0100

    Redeploy TP06 6

--- deploy/api/controllers/catalogue.controllers.js
+++ deploy/api/controllers/catalogue.controllers.js
@@ -1,18 +1,18 @@
+const catalogue = [
+  { ref: "A", name: "Smartphone", price: 899.99 },
+  { ref: "B", name: "Laptop", price: 619.99 },
+  { ref: "C", name: "Headphones", price: 299.99 },
+  { ref: "D", name: "Smartwatch", price: 199.99 },
+  { ref: "E", name: "Robot vacuum cleaner", price: 335.88 },
+  { ref: "F", name: "VR headset", price: 349.99 },
+  { ref: "G", name: "Bicycle", price: 600 },
+  { ref: "H", name: "Printer", price: 120 },
+  { ref: "I", name: "Drone", price: 199.99 },
+  { ref: "J", name: "Shower gel", price: 12.5 },
+];
+
 exports.get = (req, res) => {
   res.setHeader("Content-Type", "application/json");
-
-  const catalogue = [
-    { ref: "A", name: "Smartphone", price: 899.99 },
-    { ref: "B", name: "Laptop", price: 619.99 },
-    { ref: "C", name: "Headphones", price: 299.99 },
-    { ref: "D", name: "Smartwatch", price: 199.99 },
-    { ref: "E", name: "Robot vacuum cleaner", price: 335.88 },
-    { ref: "F", name: "VR headset", price: 349.99 },
-    { ref: "G", name: "Bicycle", price: 600 },
-    { ref: "H", name: "Printer", price: 120 },
-    { ref: "I", name: "Drone", price: 199.99 },
-    { ref: "J", name: "Shower gel", price: 12.5 },
-  ];
 
   // Articles recommandés
   res.send(catalogue);
@@ -28,19 +28,6 @@ exports.search = (req, res) => {
   }
 
   res.setHeader("Content-Type", "application/json");
-
-  const catalogue = [
-    { ref: "A", name: "Smartphone", price: 899.99 },
-    { ref: "B", name: "Laptop", price: 619.99 },
-    { ref: "C", name: "Headphones", price: 299.99 },
-    { ref: "D", name: "Smartwatch", price: 199.99 },
-    { ref: "E", name: "Robot vacuum cleaner", price: 335.88 },
-    { ref: "F", name: "VR headset", price: 349.99 },
-    { ref: "G", name: "Bicycle", price: 600 },
-    { ref: "H", name: "Printer", price: 120 },
-    { ref: "I", name: "Drone", price: 199.99 },
-    { ref: "J", name: "Shower gel", price: 12.5 },
-  ];
 
   // Filtrer
   let search = catalogue.filter(
